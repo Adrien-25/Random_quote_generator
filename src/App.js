79c0ca9d0ea09d6611623b9c2fc5a2148405127a
Adrien Schmidt <61613165+Adrien-25@users.noreply.github.com>
2024-11-02 02:26:24 +0100

    correction warning

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // src/App.js
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { fetchQuote } from "./api"; // Import de la fonction d'API
 import QuoteBox from "./components/QuoteBox";
 import QuoteButton from "./components/QuoteButton";
@@ -10,15 +10,31 @@ const App = () => {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
 
-  useEffect(() => {
-    getQuote();
-  }, []);
+  const changeBackgroundColor = useCallback(() => {
+    const randomColor1 = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+    const randomColor2 = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+      Math.random() * 256
+    )}, ${Math.floor(Math.random() * 256)})`;
+    const shadowX = Math.floor(Math.random() * 100) - 50; // Mouvement horizontal aléatoire
+    const shadowY = Math.floor(Math.random() * 200) - 100; // Mouvement vertical aléatoire
+    const blurRadius = Math.floor(Math.random() * 200) + 100; // Taille du flou aléatoire
 
-  const getQuote = async () => {
+    document.body.style.backgroundColor = randomColor1;
+    document.body.style.boxShadow = `inset ${shadowX}px ${shadowY}px ${blurRadius}px ${-50}px ${randomColor2}`;
+  }, []); // pas de dépendances ici
+
+  const getQuote = useCallback(async () => {
     const { quote, author } = await fetchQuote();
     setQuote(quote);
     setAuthor(author);
-  };
+    changeBackgroundColor();
+  }, [changeBackgroundColor]); //
+
+  useEffect(() => {
+    getQuote();
+  }, [getQuote]);
 
   return (
     <div id="quote-box">
